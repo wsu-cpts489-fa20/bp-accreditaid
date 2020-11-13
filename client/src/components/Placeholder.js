d6da791965cb622a3f60d99c76ecf3060758c0fc
Sindorman <mikebykhovtsev@gmail.com>
2020-11-13 00:54:37 -0800

    Reworking App's main color theme and updating about information.

---
+++ client/src/components/Placeholder.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Placeholder extends React.Component {
+
+    render() {
+        return (
+        <div className="padded-page">
+            <center>
+            <h1 > Placeholder </h1>
+            <h2>This page is under construction.</h2>
+            <img src="https://brand.wsu.edu/wp-content/themes/brand/images/pages/logos/wsu-signature-vertical.svg" />
+            </center>
+        </div>
+        );
+    }
+}
+
+export default Placeholder;
