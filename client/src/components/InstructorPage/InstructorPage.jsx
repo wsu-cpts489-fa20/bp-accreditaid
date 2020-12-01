3d9b9a11a43ee92c696a550d8954f2d4a5a140d2
Micah <micah.priddis@wsu.edu>
2020-11-30 22:44:17 -0800

    Created react component for the Instructor page

---
+++ client/src/components/InstructorPage/InstructorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class InstructorPage extends React.Component {
+    render() {
+        return(
+        <div>
+            Instructor page
+        </div>)
+    }
+}
+
+export default InstructorPage;
