b6e4c74cee7716be228fe9c6416afaca96f7d1e9
warior4356 <warior4356@yahoo.com>
2020-12-01 23:23:52 -0800

    Update CourseFilesTab.jsx

--- client/src/components/CourseInfo/CourseFilesTab.jsx
+++ client/src/components/CourseInfo/CourseFilesTab.jsx
@@ -7,11 +7,17 @@ class CourseFiles extends React.Component {
         this.fileInput = React.createRef();
     }
 
-    deleteFile = (id, name) => {
-        
+    deleteFile = (id, name, db_update, key, index=0) => {
+        fetch(("/api/s3?id=" + id + "&name=" + name), {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(json => console.log(json))
+        .catch(err => console.error(err))
+        .then(() => {db_update(key, index)});
     }
 
-    uploadFile = (file) => {
+    uploadFile = (file, db_update, type) => {
 
         // add file to FormData object
         const fd = new FormData();
@@ -24,13 +30,26 @@ class CourseFiles extends React.Component {
         })
         .then(res => res.json())
         .then(json => console.log(json))
+        .catch(err => console.error(err))
+        .then(() => {db_update(file, json.body.VersionId, type)});
+    }
+
+    upload_single = (file, id, type) => {
+        let body = {};
+        body[type] = {id: id, file: file};
+        fetch("/api/course/" + this.props.course._id, {
+            method: 'PUT',
+            body: body
+        })
+        .then(res => res.json())
+        .then(json => console.log(json))
         .catch(err => console.error(err));
     }
 
     onSubmit = (event) =>{
         console.log(this.fileInput.current.files[0]);
         event.preventDefault()
-        this.uploadFile(this.fileInput.current.files[0])
+        this.uploadFile(this.fileInput.current.files[0], this.upload_single, event.target.id)
         alert(
             `Selected file - ${this.fileInput.current.files[0].name}`
           );
@@ -42,7 +61,7 @@ class CourseFiles extends React.Component {
                 CourseFiles
 
                 <form onSubmit={this.onSubmit}>
-                    <input type="file" id="myFile" name="file" ref={this.fileInput}></input>
+                    <input type="file" id="courseSyllabus" name="file" ref={this.fileInput}></input>
                     <button type="submit"></button> 
                 </form>
             </div>
