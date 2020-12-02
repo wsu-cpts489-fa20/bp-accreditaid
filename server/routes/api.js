df8f11d96608df466a6aa312b71fdb7a7240f68c
warior4356 <warior4356@yahoo.com>
2020-12-01 18:53:21 -0800

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const users = require("./api/users")
 const programs = require("./api/programs")
 const courses = require("./api/courses")
+const s3 = require("./api/s3")
 
 router.get('/test', function (req, res) {
   res.status(200).json({'test': 'it works!'});
@@ -12,5 +13,6 @@ router.get('/test', function (req, res) {
 router.use("/users", users);
 router.use("/programs", programs)
 router.use("/courses", courses)
+router.use("/s3", s3)
 
 module.exports = router;
