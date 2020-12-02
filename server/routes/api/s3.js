29a6133a145e56fe28dcd311489e22b74489310d
Micah <micah.priddis@wsu.edu>
2020-12-01 19:29:40 -0800

    Partially working file uploads to /api/s3

--- server/routes/api/s3.js
+++ server/routes/api/s3.js
@@ -4,61 +4,43 @@ const Mongoose = require('mongoose')
 const AWS = require('aws-sdk');
 const fs = require('fs');
 
+const multer = require('multer');
+const upload = multer({
+  dest: 'uploads/' // this saves your file into a directory called "uploads"
+}); 
+
 AWS.config.update({region: 'us-east-1'});
 
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 var uploadParams = {Bucket: 'accreditaid', Key: '', Body: ''};
 
+
 //upload file route
 router.post('/',  async (req, res, next) => {
-    try {
-        if(!req.files) {
-            res.send({
-                status: false,
-                message: 'No file uploaded'
-            });
-        } else {
-            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-            let doc = req.files.doc;
-            
-            //Use the mv() method to place the file in upload directory (i.e. "uploads")
-            var filepath = './uploads/' + doc.name;
-            doc.mv(filepath);
-
-            //upload the file to s3
-            var fileStream = fs.createReadStream(file);
-            fileStream.on('error', function(err) {
-                console.log('File Error', err);
-            });
-
-            uploadParams.Body = fileStream;
-            var path = require('path');
-            uploadParams.Key = path.basename(filepath);
-
-            s3.upload (uploadParams, function (err, data) {
-                if (err) {
-                  console.log("Error", err);
-                } if (data) {
-                  console.log("Upload Success", data.Location);
-                }
-            });
-
-            //send response
-            res.send({
-                status: true,
-                message: 'File is uploaded',
-                data: {
-                    name: doc.name,
-                    mimetype: doc.mimetype,
-                    size: doc.size
-                }
-            });
-        }
-    } catch (err) {
-        res.status(500).send(err);
-    }
-
+        let upload = multer({ }).single('');
+    
+        upload(req, res, function(err) {
+            // req.file contains information of uploaded file
+            // req.body contains information of text fields, if there were any
+    
+            if (req.fileValidationError) {
+                return res.send(req.fileValidationError);
+            }
+            else if (!req.file) {
+                return res.send('Please select an image to upload');
+            }
+            else if (err instanceof multer.MulterError) {
+                return res.send(err);
+            }
+            else if (err) {
+                return res.send(err);
+            }
+    
+            // Display uploaded image for user validation
+            console.log(req.file);
+            res.send("File successfully uploaded");
+        });
 });
 
 module.exports = router;
