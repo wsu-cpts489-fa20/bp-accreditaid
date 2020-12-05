be43a6bbaccf219dc429cc6c120d355966f87985
Alexandros Papadopoulos <alexandrospapadopoulos@MacBook-Pro.local>
2020-12-04 19:07:16 -0800

    finished Admin deliverables tests

--- client/tests/AdminDeliverablesTests.js
+++ client/tests/AdminDeliverablesTests.js
@@ -34,12 +34,12 @@ test('TestAddDeliverable', async t => {
         .wait(300)
         .click('#add-deliverable-button')
         .wait(500)
-        .typeText('#deliverable-name', 'Deliverable Name')
-        .typeText('#deliverable-description', 'Deliverable Description')
+        .typeText('#deliverable-name', 'Deliverable 1')
+        .typeText('#deliverable-description', 'This is a deliverable.')
         .click('#deliverables-submit')
         .wait(1000)
         .expect(Selector('#deliverables-table').visible).eql(true)  
-        .expect(Selector('#deliverables-table').find('td').withText('Deliverable Name').visible).eql(true)
+        .expect(Selector('#deliverables-table').find('td').withText('Deliverable 1').visible).eql(true)
         .wait(500);
 });
 
@@ -49,12 +49,39 @@ test('TestUpdateWorkSampleLabels', async t => {
     await LoginAndGoToDeliverables();
     await t
         .wait(300)
-        .click('#edit-labels-button')
+        .click('#add-deliverable-button')
         .wait(500)
+        .typeText('#deliverable-name', 'Deliverable Name')
+        .typeText('#deliverable-description', 'Deliverable Description')
+        .click('#delete-label-2')
+        .click('#add-label')
+        .click('#add-label')
+        .wait(300)
+        .typeText('#performance-labels-2', '-A')
+        .typeText('#performance-labels-3', '-B')
+        .click('#deliverables-submit')
+        .wait(1000)
+        .expect(Selector('#deliverables-table').visible).eql(true) 
+        .expect(Selector('#deliverables-table').find('td').withText(
+            'High, Medium, None-A, None-B').visible).eql(true)
+});
+
+test('TestUpdateWorkSampleLabels', async t => {
+    await t
+        .setNativeDialogHandler(() => true);
+    await LoginAndGoToDeliverables();
+    await t
+        .wait(300)
+        .click('#add-deliverable-button')
+        .wait(500)
+        .typeText('#deliverable-name', 'Deliverable Name')
+        .typeText('#deliverable-description', 'Deliverable Description')
         .click('#delete-label-2')
         .click('#delete-label-1')
-        .click('#add-label')
-        .click('#add-label')
-        .click('#add-label')
+        .click('#delete-label-0')
+        .click('#deliverables-submit')
+        .wait(1000)
+        .expect(Selector('#deliverables-table').visible).eql(true) 
+        .expect(Selector('#deliverables-table').find('td').withText('N/A').visible).eql(true)
 });
 
